fee76bc11932b9cb78f00c42918d32afd2e3681e
it <it1.jobtechventure.com>
2024-12-11 17:50:53 +0530

    change css

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -6,7 +6,17 @@ const BlogCard = ({ blog }) => {
         <div className=" max-w-md  rounded-lg shadow-lg border border-gray-200 p-4 bg-gray-100">
             <img src={blog.image} alt={blog.title} className="w-full h-64 object-cover rounded-lg border" />
             <div className="mt-4">
-                <h2 className="text-2xl font-bold text-gray-900 mt-2">{blog.title}</h2>
+                <h2 className="text-xl font-bold text-gray-900 mt-2  pb-2 ">{blog.title}</h2>
+                <hr/>
+                <div className='flex justify-between pt-3 p-2'>
+                    <div className='flex gap-3'>
+                        <p>0 views</p>
+                        <p>0 views</p>
+                    </div>
+                    <div>
+                        9 ❤
+                    </div>
+                </div>
             </div>
         </div>
     </>
