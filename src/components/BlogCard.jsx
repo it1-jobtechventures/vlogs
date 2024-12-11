a0bc1a82f65b1d4e376f21ecfbacdb163cfaa674
it <it1.jobtechventure.com>
2024-12-11 18:00:19 +0530

    add hover effect

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BlogCard = ({ blog }) => {
   return (
     <>
-        <div className=" max-w-md  rounded-lg shadow-lg border border-gray-200 p-4 bg-gray-100">
+        <div className=" max-w-md  rounded-lg shadow-lg border border-gray-200 p-4 bg-gray-100 transition-transform transform hover:scale-105 hover:shadow-2xl">
             <img src={blog.image} alt={blog.title} className="w-full h-64 object-cover rounded-lg border" />
             <div className="mt-4">
                 <h2 className="text-xl font-bold text-gray-900 mt-2  pb-2 ">{blog.title}</h2>
